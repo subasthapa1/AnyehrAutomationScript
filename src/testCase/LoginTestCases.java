129b03a7b4358bcdc4f35e3d60012ed427b42583
subasbdrthapa <subasthapa.official@gmail.com>
2017-02-16 17:36:00 +0545

    2 more test cases

--- src/testCase/LoginTestCases.java
+++ src/testCase/LoginTestCases.java
@@ -7,13 +7,42 @@ import org.testng.annotations.Test;
 import ekbana.LoginPage;
 
 public class LoginTestCases {
+	 @Test
+	  public void verifyValidLogin() {
+		  WebDriver driver = new FirefoxDriver();
+		  driver.get("http://emr.anyehr.com/cms/login");
+		  LoginPage login= new LoginPage(driver);
+		  login.doLogin("superqa", "superqa@123");
+		  driver.quit();
+		  
+	  }
+ 
   @Test
-  public void verifyValidLogin() {
+  public void verifyInValidPassword() {
 	  WebDriver driver = new FirefoxDriver();
 	  driver.get("http://emr.anyehr.com/cms/login");
 	  LoginPage login= new LoginPage(driver);
-	  login.doLogin("superqa", "superqa@123");
+	  login.doLogin("superqa", "superqa@1234");
+	  driver.quit();
+  }
+  @Test
+  public void verifyValidPasswordAndEmail() {
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get("http://emr.anyehr.com/cms/login");
+	  LoginPage login= new LoginPage(driver);
+	  login.doLogin("superqa1", "superqa@123");
 	  driver.quit();
 	  
   }
+  @Test
+  public void verifyUnregisteredAcount() {
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get("http://emr.anyehr.com/cms/login");
+	  LoginPage login= new LoginPage(driver);
+	  login.doLogin("superman", "superqa@123");
+	  driver.quit();
+	  
+  }
+  
+  
 }
